6aa12f9cf1a051c377194587e8c30706ca9c5008
Fotis <nikolaidis.fotis@gmail.com>
2018-10-04 16:36:18 +0200

    Test code

--- code/struct1.go
+++ code/struct1.go
@@ -2,15 +2,29 @@ package main
 
 import "fmt"
 
+type Person struct {
+	Name  string
+	Actor string
+}
+
 func main() {
 	// START OMIT
-	var p = struct {
-		Name string
-		Age  int
-	}{"Baiju", 36}
-	fmt.Printf("%v\n", p)
-	fmt.Printf("%+v\n", p)
-	fmt.Printf("%#v\n", p)
-	fmt.Println("Name:", p.Name)
+	var me Person
+	me.Name = "Douglas Powers"
+	me.Actor = "Mike Myers"
+
+	minime := Person{
+		Name:  "Doublas Powers Jr.",
+		Actor: "Mike Myers",
+	}
+
+	var archenemy = struct {
+		Person
+		Nationality string
+	}{Person{"Austin Powers", "Mike Myers"}, "British"}
+
+	fmt.Printf("%v\n", me)
+	fmt.Printf("%+v\n", minime)
+	fmt.Printf("%#v\n", archenemy)
 	// END OMIT
 }
