365fa23821727d93b18791a43fa2f8f8379eebb1
Fotis <nikolaidis.fotis@gmail.com>
2018-10-13 01:52:04 +0200

    More final

--- code/functiondeclaration2.go
+++ code/functiondeclaration2.go
@@ -5,8 +5,8 @@ import "fmt"
 // START OMIT
 func main() {
 	var divident = 503
-	divideby := func(d int) (quotient int, mod int) { // HL
-		return divident / d, divident % d // HL
+	divideby := func(d int) (quotient int, mod int) { // Assign function to value // HL
+		return divident / d, divident % d // Lexically scoped name binding // HL
 	}
 	fmt.Println(divideby(2)) // HL
 }
